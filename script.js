94f9775a6d92e5464f37d10a6fc4d707d7f1fc4a
Aakansha Hujare <158164641+1234aakansha@users.noreply.github.com>
2024-07-17 16:15:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let guessNum=5;
+let userNum=prompt("Please Enter any Number from 1 to 100:");
+
+while(guessNum != userNum)
+    {
+        userNum=prompt("You gussed wrong Number.Please guessed right number");
+
+}
+console.log("Congratulations,you enter right number");
